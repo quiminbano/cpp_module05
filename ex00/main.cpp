7a35071907645906eb481f33f62fcdc75a440a77
Carlos Orellana <corellan@c2r3p6.hive.fi>
2023-06-19 14:24:38 +0300

    ex00 finished

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,84 @@
 /*   By: corellan <corellan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 10:42:17 by corellan          #+#    #+#             */
-/*   Updated: 2023/06/18 19:06:06 by corellan         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:20:04 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "GradeTooHighException.hpp"
+#include "GradeTooLowException.hpp"
+#include "ParameterMissing.hpp"
 
 int	main(void)
 {
+	Bureaucrat	emilia("Emilia", 150);
+	Bureaucrat	matias("Matias", 1);
+	Bureaucrat	rosa("Rosa", 50);
+
+	std::cout << emilia << std::endl;
+	std::cout << matias << std::endl;
+	std::cout << rosa << std::endl;
 	try
 	{
-		Bureaucrat	emilia("Emilia");
+		emilia.decrementGrade(1);
 	}
 	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		matias.incrementGrade(1);
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		rosa.incrementGrade(100);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	john;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	jimmy("Jimmy");
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	jhonny(150);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	jhonny2(0);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	jhonny3(151);
+	}
+	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
