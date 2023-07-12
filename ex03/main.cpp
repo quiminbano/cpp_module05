ef511e18aca888b05356b041b6520700a821cd58
Carlos Orellana <corellan@c3r3p4.hive.fi>
2023-07-12 11:26:00 +0300

    ex03 finally finished

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 13:51:52 by corellan          #+#    #+#             */
-/*   Updated: 2023/06/26 10:45:11 by corellan         ###   ########.fr       */
+/*   Updated: 2023/07/12 11:22:20 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,16 @@ int	main(void)
 	AForm		*carlos;
 	Intern		hiver;
 
+	std::cout << std::endl;
 	tree = hiver.makeForm("ShrubberyCreationForm", "hive");
 	roboto = hiver.makeForm("RobotomyRequestForm", "bocal");
 	president = hiver.makeForm("PresidentialPardonForm", "madonna");
+
+	std::cout << std::endl;
 	carlos = hiver.makeForm("GenericForm", "myHouse");
 	std::cout << "Carlos Pointer: " << carlos << std::endl;
+
+	std::cout << std::endl;
 	tree->beSigned(emilia);
 	emilia.signForm(*tree);
 	try
@@ -42,10 +47,14 @@ int	main(void)
 		std::cerr << e.what() << std::endl;
 	}
 	emilia.executeForm(*tree);
+
+	std::cout << std::endl;
 	tree->beSigned(matias);
 	matias.signForm(*tree);
 	tree->execute(matias);
 	matias.executeForm(*tree);
+
+	std::cout << std::endl;
 	roboto->beSigned(rosa);
 	rosa.signForm(*roboto);
 	try
@@ -57,10 +66,14 @@ int	main(void)
 		std::cerr << e.what() << std::endl;
 	}
 	rosa.executeForm(*roboto);
+
+	std::cout << std::endl;
 	roboto->beSigned(matias);
 	matias.signForm(*roboto);
 	roboto->execute(matias);
 	matias.executeForm(*roboto);
+
+	std::cout << std::endl;
 	matias.decrementGrade(10);
 	president->beSigned(matias);
 	matias.signForm(*president);
@@ -74,10 +87,12 @@ int	main(void)
 	}
 	matias.executeForm(*president);
 	matias.incrementGrade(10);
-	president->beSigned(matias);
-	matias.signForm(*president);
+
+	std::cout << std::endl;
 	president->execute(matias);
 	matias.executeForm(*president);
+
+	std::cout << std::endl;
 	delete tree;
 	delete roboto;
 	delete president;
